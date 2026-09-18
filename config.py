2964a7e6eab68d336fc82275ad0e58f2aca198ba
Antonio <salomontocon@gmail.com>
2026-09-18 10:47:59 -0600

    cambios css

--- config.py
+++ config.py
@@ -1,6 +1,17 @@
+import os
+
 class Config:
-    MYSQL_HOST = 'localhost'        
-    MYSQL_USER = 'root'             
-    MYSQL_PASSWORD = '123456'  
-    MYSQL_DB = 'sys_bodega'   
-    MYSQL_PORT = 3306  
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'clave-secreta-corporacion-merino'
+
+    # Credenciales MySQL
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = '123456'
+    MYSQL_DB = 'firmas'
+    MYSQL_PORT = 3306
+
+    # Cadena de conexión SQLAlchemy para MySQL mediante PyMySQL
+    SQLALCHEMY_DATABASE_URI = (
+        f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
